6d604d930bc631cfcfd4b02aff101ba6a5f428ad
Rana-Mudassir <mduasire38@gmail.com>
2023-06-07 18:54:16 +0500

    Fixed Linters errors

--- person.rb
+++ person.rb
@@ -1,23 +1,23 @@
 class Person
-    def initialize( name, age = "Unknown" , parent_permission: true )
-       @id = Random.rand(1..1000)
-       @name = name
-       @age = age
-       @parent_permission = parent_permission
-    end
+  def initialize(name, age = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    attr_reader :id
-    attr_accessor :name, :age, :parent_permission
-    
-    private
+  attr_reader :id
+  attr_accessor :name, :age, :parent_permission
 
-    def of_age?
-        @age >= 18
-    end
+  private
 
-    public 
+  def of_age?
+    @age >= 18
+  end
 
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
--- student.rb
+++ student.rb
@@ -1,14 +1,14 @@
 require_relative 'person'
 
 class Student < Person
-    attr_reader :classroom, :parent_permission
+  attr_reader :classroom, :parent_permission
 
-    def initialize(name = 'unknown', age, parent_permission: true, classroom: nil)
-        super(name, age, parent_permission: parent_permission)
-        @classroom = classroom
-    end
+  def initialize(age, name = 'unknown', parent_permission: true, classroom: nil)
+    super(name, age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
