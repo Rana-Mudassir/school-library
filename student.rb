3a0881b8da9dbcf88b05e2c8c3f9291e27315d31
Rana-Mudassir <mduasire38@gmail.com>
2023-06-10 01:39:11 +0500

    Add class class and association with student

--- student.rb
+++ student.rb
@@ -1,11 +1,19 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   attr_reader :classroom, :parent_permission
 
   def initialize(age, name = 'unknown', parent_permission: true, classroom: nil)
     super(name, age, parent_permission: parent_permission)
+    self.classroom = classroom
+  end
+
+  def classroom(classroom)
+    return if @classroom == classroom
+    @classroom&.students&.delete(self)
     @classroom = classroom
+    classroom.students << self
   end
 
   def play_hooky
