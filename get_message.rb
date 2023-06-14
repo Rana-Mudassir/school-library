833239bb0883b3efd56742967eebc007cc73773a
dieum512 <akonkwadieum222@gmail.com>
2023-06-14 10:02:53 +0200

    Correct the linter errors

--- get_message.rb
+++ get_message.rb
@@ -1,7 +1,7 @@
 class GetMessage
   def get_request(message, input_type = :to_s)
-      print message
-      input = gets.chomp
-      input_type == :to_i ? input.to_i : input
+    print message
+    input = gets.chomp
+    input_type == :to_i ? input.to_i : input
   end
 end
