ef146ece14478ae896ab9bff1a01d186b9444393
dieum512 <akonkwadieum222@gmail.com>
2023-06-14 09:44:11 +0200

    Refactor the main.rb file

--- main.rb
+++ main.rb
@@ -1,37 +1,24 @@
 require_relative 'app'
+require_relative 'options'
+require_relative 'menu_choice'
+require_relative 'get_message'
 
 class Main
-  def initialize
-    @app = App.new
-  end
-
-  def list_options
-    actions = [
-      '1 - List all books',
-      '2 - List all people',
-      '3 - Create a person',
-      '4 - Create a book',
-      '5 - Create a rental',
-      '6 - List all rentals for a given person id',
-      '7 - Exit'
-    ]
-    puts actions
-  end
-  puts 'Welcome, choose an option below:\n'
-  def get_request(message, input_type = :to_s)
-    print message
-    input = gets.chomp
-    input_type == :to_i ? input.to_i : input
+  def initialize(app)
+    @app = app
+    @list_options = Option.new
+    @execute_action = MenuChoice.new(@app)
+    @get_request = GetMessage.new
   end
 
   def main
     loop do
-      list_options
-      choice = get_request('Enter your choice: ', :to_i)
+      @list_options.list_options
+      choice = @get_request.get_request('Enter your choice: ', :to_i)
 
       case choice
       when 1..6
-        execute_action(choice)
+        @execute_action.execute_action(choice)
       when 7
         break
       else
@@ -39,49 +26,6 @@ class Main
       end
     end
   end
-
-  def execute_action(choice)
-    case choice
-    when 1
-      list_books
-    when 2
-      list_people
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals_for_person
-    end
-  end
-
-  def list_people
-    @app.list_people
-  end
-
-  def list_books
-    @app.list_books
-  end
-
-  def create_book
-    @app.create_book
-  end
-
-  def create_rental
-    @app.create_rental
-  end
-
-  def create_person
-    @app.create_person
-  end
-
-  def list_rentals_for_person
-    person_id = get_request('Enter person ID: ', :to_i)
-    @app.list_rentals(person_id)
-  end
 end
 
-main_app = Main.new
-main_app.main
+Main.new(App.new).main
